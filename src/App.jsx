34a48a01a6ffe885ea7739cfb61994076d58e66d
pkthunder87 <pkthunder87@gmail.com>
2023-09-19 06:48:22 -0700

    Lesson 349: Fetching Cabin Data

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ import AppLayout from "./ui/AppLayout.jsx";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000,
+      // staleTime: 60 * 1000,
+      staleTime: 0,
     },
   },
 });
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,21 +1,19 @@
-import { useEffect } from "react";
+import CabinTable from "../features/cabins/CabinTable.jsx";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins.js";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src="https://kbkxmikpvzszvtvmtsbk.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
-        alt="Cozy wooden cabin"
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
