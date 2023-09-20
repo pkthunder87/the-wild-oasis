2e26c44c8cca42ba5ea63e44fc54d98962f66abd
pkthunder87 <pkthunder87@gmail.com>
2023-09-20 09:18:00 -0700

    Lesson 355: Uploading Images to Supabase

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -64,7 +64,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {
@@ -144,9 +144,14 @@ function CreateCabinForm() {
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
+      <FormRow label="Cabin photo">
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register("image", {
+            required: "This field is required",
+          })}
+        />
       </FormRow>
 
       <FormRow>
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://kbkxmikpvzszvtvmtsbk.supabase.co";
+export const supabaseUrl = "https://kbkxmikpvzszvtvmtsbk.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtia3htaWtwdnpzenZ0dm10c2JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUxMDg0NDgsImV4cCI6MjAxMDY4NDQ0OH0.d3xHF48psVdvrsNkxWgjy3-9Y3AEgX7vGRXpV7Cfft0";
 const supabase = createClient(supabaseUrl, supabaseKey);
