06ad5b305b2d0242bb73ddbe1398bd47d110891f
pkthunder87 <pkthunder87@gmail.com>
2023-09-27 16:24:01 -0700

    Build: Add path resolver to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(
+        "/Users/Volt/webdev/react-course/17-the-wild-oasis/the-wild-oasis",
+        "./src"
+      ),
+      "@tests": path.resolve(
+        "/Users/Volt/webdev/react-course/17-the-wild-oasis/the-wild-oasis",
+        "./tests"
+      ),
+    },
+  },
+});
